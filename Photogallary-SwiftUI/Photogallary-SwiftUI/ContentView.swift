12e8c2efc37d5a2e12d6d3310793be0084e20ddb
Eugene Berezin <35341885+Eugeneberezin@users.noreply.github.com>
2021-04-20 18:00:08 -0700

    6:00 fetch photos

--- Photogallary-SwiftUI/Photogallary-SwiftUI/ContentView.swift
+++ Photogallary-SwiftUI/Photogallary-SwiftUI/ContentView.swift
@@ -15,6 +15,13 @@ struct ContentView: View {
         VStack {
             List {
                 
+                ForEach(photos) { photo in
+                    photo.photo
+                        .resizable()
+                        .aspectRatio(1, contentMode: .fill)
+                        .border(Color.white)
+                }
+                
             }
         }
         .onAppear(perform: {
@@ -22,12 +29,49 @@ struct ContentView: View {
         })
     }
     
+    func fetchPhotos() {
+        let imgManager = PHImageManager.default()
+        let requestOptions = PHImageRequestOptions()
+        requestOptions.isSynchronous = true
+        requestOptions.deliveryMode = .highQualityFormat
+        
+        let fetchOptions = PHFetchOptions()
+        fetchOptions.sortDescriptors = [NSSortDescriptor(key: "creationDate", ascending: true)]
+        
+        let fetchResult: PHFetchResult = PHAsset.fetchAssets(with: .image, options: fetchOptions)
+        
+        if fetchResult.count > 0 {
+            
+            for i in 0 ..< fetchResult.count {
+                imgManager.requestImage(
+                    for: fetchResult.object(at: i), targetSize: CGSize(width: 100, height: 200),
+                    contentMode: .aspectFit,
+                    options: requestOptions) { (image, _) in
+                    
+                    if let image = image {
+                       
+                            let photo = Photo(photo: Image(uiImage: image))
+                            photos.append(photo)
+                    }
+                }
+            }
+        } else {
+            DispatchQueue.main.async {
+                //isPhotosEmpty = true
+            }
+        }
+        
+    }
+    
     func requestAuthorizationAndFetchPhotos() {
         PHPhotoLibrary.requestAuthorization { status in
             switch status {
             
             case .authorized:
-                break
+                DispatchQueue.main.async {
+                    fetchPhotos()
+                }
+                
             
             default:
                 break
